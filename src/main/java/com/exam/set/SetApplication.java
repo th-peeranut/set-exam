6882840558bda123be260ebfca18efe2d98e7e06
peeranut.t <peeranut.t@entronica.in.th>
2025-02-27 10:18:53 +0700

    Initial project

---
+++ src/main/java/com/exam/set/SetApplication.java
@@ -0,0 +1,13 @@
+package com.exam.set;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/set/SetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
